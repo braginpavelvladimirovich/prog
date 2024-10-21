a5a224b7ecd565a71c7bcfbb529604979d080f93
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-10-21 15:34:16 +0700

    2024.10.21 data types java char number to symbol and string format testing

--- pr001.java
+++ pr001.java
@@ -16,7 +16,11 @@ class pr001 {
 		c = (char)133;
 		System.out.println(c);
 		//Non-primitive data types - such as String, Arrays and Classes
-		String str;
+		String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		System.out.println("The length of the txt string is: " + txt.length());
+		txt = "Hello World";
+		System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+		System.out.println(txt.toLowerCase());	 // System.out.println(txt.toLowerCase());   // Outputs "hello world"
 		//
 	}
 }
