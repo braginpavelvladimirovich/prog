31b5aa78bbc87aec9ba9208c6100d8160930e365
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-10-31 13:06:51 +0700

    2024.10.31 Bragin Int value table add

--- bragin.java
+++ bragin.java
@@ -1,7 +1,30 @@
 class bragin {
 	public static void main(String args[]) {
+		System.out.println("\n<\t<\t<\t<\tB R A G I N\t>\t>\t>\t>\t\n");
+		System.out.println("================================================================================================================================");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		long k = 9223372036854775807L;
+		long l = -9223372036854775808L;
 		int i = 2147483647;
 		int j = -2147483648;
-		System.out.println("\tInt 32 bit number between:\t" + i + "\tand\t" + j + "\t\n");
+		short s = 32767;
+		short h = -32768;
+		byte a = 127;
+		byte b = -128;
+		System.out.println("|\tLong int 64 bit number between:\t\t" + k + "\tand\t" + l + "\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\tInt 32 bit number between:\t\t" + i + "\tand\t" + j + "\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\tShort int 16 bit number between:\t" + s + "\tand\t" + h + "\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\tByte 8 bit number between:\t\t" + a + "\tand\t" + b + "\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("================================================================================================================================");
+		System.out.println("\n<\t<\t<\t<\tE N D\t>\t>\t>\t>\t\n");
 	}
 }
