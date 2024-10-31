289f7a266eafbef7e4bca7d2653022ca5dc2711f
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-10-31 13:49:08 +0700

    2024.10.31 Bragin table add boolean char float double

--- bragin.java
+++ bragin.java
@@ -12,6 +12,14 @@ class bragin {
 		short h = -32768;
 		byte a = 127;
 		byte b = -128;
+		double d1 = Double.longBitsToDouble(0x7fefffffffffffffL);//double null Double.longBitsToDouble(0x0000000000000000)
+		double d2 = Double.longBitsToDouble(0xffefffffffffffffL);
+		float f1 = Float.intBitsToFloat(0x7f7fffff);//float null Float.intBitsToFloat(0x00000000)
+		float f2 = Float.intBitsToFloat(0xff7fffff);
+		char c1 = 0;
+		char c2 = 65535;
+		boolean b1 = true;
+		boolean b2 = false;
 		System.out.println("|\tLong int 64 bit number between:\t\t" + k + "\tand\t" + l + "\t\t\t        |");
 		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
 		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
@@ -22,6 +30,18 @@ class bragin {
 		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
 		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
 		System.out.println("|\tByte 8 bit number between:\t\t" + a + "\tand\t" + b + "\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\tDouble 64 bit number between:\t\t" + d1 + "\tand\t" + d2 + "\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\tFloat 32 bit number between:\t\t" + f1 + "\tand\t" + f2 + "\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\tChar int 16 bit number between:\t\t" + (int)c1 + "\tand\t" + (int)c2 + "\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
+		System.out.println("|\tBoolean 1 bit value between:\t\t" + b1 + "\tand\t" + b2 + "\t\t\t\t\t\t\t        |");
 		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
 		System.out.println("|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t        |");
 		System.out.println("================================================================================================================================");
