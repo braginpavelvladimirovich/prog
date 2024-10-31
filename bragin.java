4bd68c0797a22a6924078fef606d195498521ab0
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-10-31 12:32:17 +0700

    2024.10.31 Bragin Int value test

---
+++ bragin.java
@@ -0,0 +1,7 @@
+class bragin {
+	public static void main(String args[]) {
+		int i = 2147483647;
+		int j = -2147483648;
+		System.out.println("\tInt 32 bit number between:\t" + i + "\tand\t" + j + "\t\n");
+	}
+}
