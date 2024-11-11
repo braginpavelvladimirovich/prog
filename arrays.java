461857ca3afbe47826e1ad4be55d145471acb08c
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-11-11 13:09:47 +0700

    2024.11.11 arrays work nums1 change 3

--- arrays.java
+++ arrays.java
@@ -24,11 +24,12 @@ class arrays {
 	min = max = nums[0];
 	for (int i = 0; i < 10; i++) {
 		if(nums[i] < min) min = nums[i];
-		if(nums[i] < max) max = nums[i];
+		if(nums[i] > max) max = nums[i];
 	}
 	System.out.println("\n\tMinimun is:\t" + min + "\n\tMaximum is:\t" + max);
 	System.out.println("\n\tEND FUNCTION\n");
 	//alteranative
+	max = min = 0;
 	int x = 10;
 	int[] nums1 = {0,1,2,3,4,5,6,7,8,9};
 	for (int i = 0; i < 10; i++)
@@ -36,7 +37,7 @@ class arrays {
 	System.out.println("\n\tEND FUNCTION\n");
 	for (int i = 0; i < 10; i++) {
 		if(nums1[i] < min) min = nums1[i];
-		if(nums1[i] < max) max = nums1[i];
+		if(nums1[i] > max) max = nums1[i];
 	}
 	System.out.println("\n\tMinimun is:\t" + min + "\n\tMaximum is:\t" + max);
 	System.out.println("\n\tEND FUNCTION\n");
