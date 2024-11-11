92e19bca37d1220d1ee9a16a07bcf861c2c02353
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-11-11 16:17:39 +0700

    2024.11.11 queue commit basic

--- q.java
+++ q.java
@@ -15,7 +15,7 @@ class q {
 		c[putloc++] = ch;
 	}
 	char get() {
-		if(getloc=putloc) {
+		if(getloc==putloc) {
 			System.out.println("\n\tqueue is empty\n");
 			return (char) 0;
 		}
@@ -30,13 +30,27 @@ class qdemo {
 		char ch;
 		int i;
 		System.out.println("\n\tsave alphabet to big queue\n");
-		for(i=0;i<25;i++)
+		for(i=0;i<26;i++)
 			bigq.put((char) ('A' + i));
 		System.out.println("\n\tqueue in big queue:\t");
-		for(i=0;i<25;i++) {
+		for(i=0;i<26;i++) {
 			ch = bigq.get();
 			if(ch != (char) 0)
-				System.out.print(ch);
+				System.out.print(" " + ch);
+		}
+		System.out.println("\n\tEND FUNCTION\n");
+		System.out.println("\n\tsave alphabet to small queue\n");
+		for(i=0;i<5;i++) {
+			System.out.print("save:\t" + (char) ('Z' - i));
+			smallq.put((char) ('Z' - i));
+			System.out.println();
+		}
+		System.out.println("\n\tEND FUNCTION\n");
+		System.out.println("\n\tget from small queue\n");
+		for(i=0;i<5;i++) {
+			ch =smallq.get();
+			if(ch != (char) 0)
+				System.out.print("\t" + ch);
 		}
 		System.out.println("\n\tEND FUNCTION\n");
 		System.out.println("\n\tEND\n");
