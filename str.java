a51b24cd9e52431272b65fa976e7d13284125e58
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-11-11 16:57:38 +0700

    2024.11.11 string

---
+++ str.java
@@ -0,0 +1,11 @@
+class str {
+	public static void main(String[] args) {
+		System.out.println("\n\tSTART\n");
+		String str1 = new String("String#1");
+		String str2 = new String(str1);
+		String str3 = "String#3";
+		System.out.println(str1 + "\n" + str2 + "\n" +str3 + "\n");
+		System.out.println("\n\tFunction stopped\n");
+		System.out.println("\n\tEND\n");
+	}
+}
