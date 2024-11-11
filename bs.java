b84105158fd3f6d7da7198ee91421495730e6e12
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-11-11 13:15:08 +0700

    2024.11.11 bubble sort

---
+++ bs.java
@@ -0,0 +1,12 @@
+//bubble sort
+class bs {
+	public static void main(String[] args) {
+		System.out.println("\n\tSTART\n");
+		int[] nums = {0,1,2,3,4,5,6,7,8,9};
+		int a,b,t,size=10;
+		System.out.print("Array:\t");
+		for(int i=0;i<size;i++)
+			System.out.print(" " + nums[i]);
+		System.out.println("\n\tEND\n");
+	}
+}
