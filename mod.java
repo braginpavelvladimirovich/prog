41b2f6919b2c708ed5259dbddde7b7450c082538
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-11-18 16:15:12 +0700

    2024.11.18 mod

---
+++ mod.java
@@ -0,0 +1,12 @@
+class AccessDemo {
+	private int alpha;
+	public int beta;
+	int gamma;
+class mod {
+	public static void main ( String [] args ) {
+		System.out.println("\n\tSTART\n");
+		ttt
+		System.out.println("\n\tFunction stopped\n");
+		System.out.println("\n\tEND\n");
+	}
+}
