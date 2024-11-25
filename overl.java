5ff20ce6c68a615338998bbaf53524db1d49cb2f
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-11-25 14:19:56 +0700

    2024.11.25 added new data and new output 2

---
+++ overl.java
@@ -0,0 +1,12 @@
+class overl {
+	void ovldemo() {
+		System.out.println("\n\tSTART\n");
+		System.out.println("\n\tno parameter\n");
+	}
+	void ovldemo(int a) {
+		System.out.println("\n\tone paratmeter type int:\t" + a);
+	}
+	void ovldemo(int a, int b) {
+		System.out.println("\n\ttwo paratmeter type int:\t" + a + "\t" + b);
+	}
+}
