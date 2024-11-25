73b3cc49b18d1f8d730799bca9fdde9676a15926
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-11-25 14:29:51 +0700

    2024.11.25 overload

--- overl.java
+++ overl.java
@@ -1,4 +1,4 @@
-class overl {
+class overload {
 	void ovldemo() {
 		System.out.println("\n\tSTART\n");
 		System.out.println("\n\tno parameter\n");
@@ -6,7 +6,28 @@ class overl {
 	void ovldemo(int a) {
 		System.out.println("\n\tone paratmeter type int:\t" + a);
 	}
-	void ovldemo(int a, int b) {
+	int ovldemo(int a, int b) {
 		System.out.println("\n\ttwo paratmeter type int:\t" + a + "\t" + b);
+		return a+b;
+	}
+	double ovldemo(double a,double b) {
+		System.out.println("\n\ttwo paratmeter type double:\t" + a + "\t" + b);
+		return a+b;
+	}
+}
+class overl {
+	public static void main (Strint [] args) {
+		overload ob = new overload();
+		int i;
+		double d;
+		System.out.println("\n\tob.ovldemo();\n");
+		ob.ovldemo();
+		System.out.println("\n\ti = ob.ovldemo(3,7);\n");
+		i = ob.ovldemo(3,7);
+		ob.ovldemo(6);
+		System.out.println("\n\tob.ovldemo(6);\n");
+		d = ob.ovldemo(3.5,8.5);
+		ob.ovldemo();
+		System.out.println("\n\td = ob.ovldemo(3.5,8.5);\n");
 	}
 }
