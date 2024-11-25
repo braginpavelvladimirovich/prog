3988563f901ebd4113213fe4bbad5311ef00b9a4
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-11-25 13:57:30 +0700

    2024.11.25 added new data and new output

--- pr000.java
+++ pr000.java
@@ -40,16 +40,16 @@ class byref {
 }
 class errormsg {
 	String[] msgs = {
-		"error print",
-		"error input",
-		"error disk",
-		"out of index"
+		"\terror print",
+		"\terror input",
+		"\terror disk",
+		"\tout of index"
 	};
 	String geterrormsg(int i){
 		if(i>=0 & i < msgs.length)
 			return msgs[i];
 		else
-			return "unclear";
+			return "\tunclear";
 	}
 }
 class err {
@@ -62,37 +62,43 @@ class err {
 }
 class errorinfo {
 	String[] msgs = {
-		"error print",
-		"error input",
-		"error disk",
-		"out of index"
+		"\terror print",
+		"\terror input",
+		"\terror disk",
+		"\tout of index"
 	};
 	int[] howbad = {3,3,2,4};
 	err geterrorinfo(int i) {
 		if(i >= 0 & i <msgs.length)
 			return new err(msgs[i],howbad[i]);
 		else
-			return new err("unclear",0);
+			return new err("\tunclear",0);
 	}
 }
 class pr000 {
 	public static void main(String[] args){
 		System.out.println("\n\tSTART\n");
+		System.out.println("\t12:21 PM 11/25/2024");
+		System.out.println("\tнеделя номер 13");
+		System.out.println("\tа. 308");
+		System.out.println("\t16-я пара: 12.10 – 13.40;");
+		System.out.println("\t17-я пара: 14.10 – 15.40;");
+		System.out.println("\t18-я пара: 15.50 – 17.20;\n");
 		block ob1 = new block(10,2,5);
 		block ob2 = new block(10,2,5);
 		block ob3 = new block(5,4,5);
-		System.out.println("ob1 and ob2 equal:\t" + ob1.sameblock(ob2));
-		System.out.println("ob1 and ob3 equal:\t" + ob1.sameblock(ob3));
-		System.out.println("ob1 and ob3 value equal:\t" + ob1.samev(ob3));
+		System.out.println("\tob1 and ob2 equal:\t" + ob1.sameblock(ob2));
+		System.out.println("\tob1 and ob3 equal:\t" + ob1.sameblock(ob3));
+		System.out.println("\tob1 and ob3 value equal:\t" + ob1.samev(ob3));
 		byvalue ob4 = new byvalue();
 		int a = 15, b =20;
-		System.out.println("before:\t" + a + "\t" + b);
+		System.out.println("\tbefore:\t" + a + "\t" + b);
 		ob4.nochange(a,b);
-		System.out.println("nochange:\t" + a + "\t" + b);
+		System.out.println("\tnochange:\t" + a + "\t" + b);
 		byref ob5 = new byref(15,20);
-		System.out.println("ob5 before:\t" + ob5.a + "\t" + ob5.b);
+		System.out.println("\tob5 before:\t" + ob5.a + "\t" + ob5.b);
 		ob5.change(ob5);
-		System.out.println("ob5 change:\t" + ob5.a + "\t" + ob5.b);
+		System.out.println("\tob5 change:\t" + ob5.a + "\t" + ob5.b);
 		System.out.println("\n\tEND FUNCTION\n");
 		//demo return string by method
 		errormsg err0 =new errormsg();
