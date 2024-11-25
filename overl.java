c19d3ae47f384ded5de39a6747a74d00abcd727d
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-11-25 14:56:46 +0700

    2024.11.25 overload demo

--- overl.java
+++ overl.java
@@ -1,4 +1,21 @@
 class overload {
+	int s;
+	overload() {
+		System.out.println("\n\tin constructor w/o param\n");
+		s=0;
+	}
+	overload(int i) {
+		System.out.println("\n\tin constructor 1 param\n");
+		s=i;
+	}
+	overload(double i) {
+		System.out.println("\n\tin constructor 1 param\n");
+		s = (int) i;
+	}
+	overload(int i, int j) {
+		System.out.println("\n\tin constructor 2 param\n");
+		s = i + j;
+	}
 	void ovldemo() {
 		System.out.println("\n\tSTART\n");
 		System.out.println("\n\tno parameter\n");
@@ -13,6 +30,15 @@ class overload {
 	double ovldemo(double a,double b) {
 		System.out.println("\n\ttwo paratmeter type double:\t" + a + "\t" + b);
 		return a+b;
+	}
+	void f(int x) {
+		System.out.println("\n\tf int:\t" + x);
+	}
+	void f(double x) {
+		System.out.println("\n\tf double:\t" + x);
+	}
+	void f(byte x) {
+		System.out.println("\n\tf byte:\t" + x);
 	}
 }
 class overl {
@@ -29,5 +55,25 @@ class overl {
 		d = ob.ovldemo(3.5,8.5);
 		ob.ovldemo();
 		System.out.println("\n\td = ob.ovldemo(3.5,8.5);\n");
+		int i2 =10;
+		double d2 = 99.9;
+		byte b =30;
+		short s =23;
+		float f=11.2f;
+		ob.f(i2);
+		ob.f(d2);
+		ob.f(b);
+		ob.f(s);
+		ob.f(f);
+		overload t1 = new overload();
+		overload t2 = new overload(3);
+		overload t3 = new overload(25.5);
+		overload t4 = new overload(5,7);
+		System.out.println("\n\tobj\t" + t2.s);
+		System.out.println("\n\tobj\t" + t2.s);
+		System.out.println("\n\tobj\t" + t3.s);
+		System.out.println("\n\tobj\t" + t4.s);
+		
+		System.out.println("\n\tEND\n");
 	}
 }
