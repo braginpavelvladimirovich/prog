5c12f4ab7bb6f2901fa57bb101ee242b78490d7d
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-11-25 12:55:54 +0700

    2024.11.25 ob5

--- pr000.java
+++ pr000.java
@@ -1,3 +1,4 @@
+//peredacha obj as args and return obj metod
 class block {
 	private int a,b,c;
 	private int v;
@@ -26,8 +27,20 @@ class byvalue {
 		j=-j;
 	}
 }
+class byref {
+	int a,b;
+	byref(int i, int j){
+		a=i;
+		b=j;
+	}
+	void change(byref ob) {
+		ob.a = ob.a +ob.b;
+		ob.b = -ob.b;
+	}
+}
 class pr000 {
 	public static void main(String[] args){
+		System.out.println("\n\tSTART\n");
 		block ob1 = new block(10,2,5);
 		block ob2 = new block(10,2,5);
 		block ob3 = new block(5,4,5);
@@ -36,8 +49,14 @@ class pr000 {
 		System.out.println("ob1 and ob2 value equal:\t" + ob1.samev(ob2));
 		byvalue ob4 = new byvalue();
 		int a = 15, b =20;
-		System.out.println("an before:\t" + a + "\t" + b);
+		System.out.println("before:\t" + a + "\t" + b);
 		ob3.nochange(a,b);
-		System.out.println("an before:\t" + a + "\t" + b);
+		System.out.println("nochange:\t" + a + "\t" + b);
+		byref ob5 = new byref(15,20);
+		System.out.println("ob5 before:\t" + ob5.a + "\t" + ob5.b);
+		ob5.change(a,b);
+		System.out.println("ob5 change:\t" + ob5.a + "\t" + ob5.b);
+		System.out.println("\n\tEND FUNCTION\n");
+		System.out.println("\n\tEND\n");
 	}
 }
