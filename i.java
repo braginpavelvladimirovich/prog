81a4274eb57f79267411502ef4bf4720ec20c436
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-12-02 16:44:53 +0700

    2024.12.02 вызов конструктора суперкласса

--- i.java
+++ i.java
@@ -1,31 +1,69 @@
 class tds {
 	double width;
 	double height;
+	String n;
+	tds(double w, double h) {
+		width = w;
+		height = h;
+		n = "Two Dimesion Shape";
+	}
 	void sd() {
 		System.out.println("\n\tdimesions:\t" + width + "\tx\t" + height);
 	}
 }
 class t extends tds {
-	String s;
+	String st;
+	t(String s, double w, double h){
+		super(w,h);
+		st = s;
+		n = "Triangle";
+	}
 	double area() {
 		return width * height / 2 ;
 	}
 	void ss() {
-		System.out.println("\n\tstyle:\t" + s);
+		System.out.println("\n\tstyle:\t" + st);
 	}
 }
 class r extends tds {
-	String o;
+	String ol;
+	r(String o, double w, double h){
+		super(w,h);
+		ol = o;
+		n = "Rectangle";
+	}
 	double area() {
 		return width * height ;
+	}
+	boolean is() {
+		if (width==height)
+			return true;
+		else
+			return false;
 	}
 }
 class i {
 	public static void main(String[] args) {
-		tds f1 = new tds();
-		t t1 = new t();
-		t t2 = new t();
-		r r1 = new r();
-		r r2 = new r();
+		tds f1 = new tds(20.0, 30.0);
+		t t1 = new t("asd",4.5,7.0);
+		t t2 = new t("contur",3.5,8.0);
+		r r1 = new r("line",5.0,5.0);
+		r r2 = new r("punkt",5.0,7.0);
+		System.out.println("info:");
+		System.out.println("f1:\t" + f1.n);
+		f1.sd();
+		System.out.println("t1:\t" + t1.n);
+		t1.ss();
+		System.out.println("area:\t" + t1.area());
+		System.out.println("t2:\t" + t2.n);
+		t2.ss();
+		System.out.println("area:\t" + t2.area());
+		System.out.println("r1:\t" + r1.n);
+		System.out.println("square?:\t" + r1.is());
+		System.out.println("area:\t" + r1.area());
+		System.out.println("r2:\t" + r2.n);
+		System.out.println("square?:\t" + r2.is());
+		System.out.println("area:\t" + r2.area());
+
 	}
 }
