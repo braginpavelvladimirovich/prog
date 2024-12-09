0d328a27c9711ae2799a8f010bcea2686e26171b
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-12-09 15:34:37 +0700

    2024.12.09 colored

--- i.java
+++ i.java
@@ -108,6 +108,8 @@ class i {
 		t2.sc();
 		System.out.println("\tarea:\t" + f1.area());
 		System.out.println("\n\tSTOP FUNCTION\t\n");
+		System.out.println("\tname:\t" + f1.name);
+		System.out.println("\n\tSTOP FUNCTION\t\n");
 		System.out.println((int)0x7fffffff);
 		System.out.println(Float.intBitsToFloat(0x7f7fffff));
 		System.out.println((int)Float.intBitsToFloat(0x7f7fffff)==(int)0x7fffffff);
