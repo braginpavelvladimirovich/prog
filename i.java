515a0f1eb9a2b9985e544cbfd9ad90402ea97393
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-12-09 16:20:31 +0700

    2024.12.09 abstract message final

--- i.java
+++ i.java
@@ -126,12 +126,12 @@ class i {
 		System.out.println(Float.intBitsToFloat(0x7f7fffff));
 		System.out.println((int)Float.intBitsToFloat(0x7f7fffff)==(int)0x7fffffff);
 		System.out.println((int)Float.intBitsToFloat(0xff7fffff)==(int)0x80000000);
-		System.out.println(Float.intBitsToFloat((int)0x7fffffff));
-		System.out.println(Float.intBitsToFloat((int)0x80000000));
+		System.out.println("\t7fffffff\t" + Float.intBitsToFloat((int)0x7fffffff));
+		System.out.println("\t80000000\t" + Float.intBitsToFloat((int)0x80000000));
 		System.out.println(Float.intBitsToFloat((int)0x7ffffffe));
 		System.out.println(Float.intBitsToFloat((int)0x80000001));
-		System.out.println(Float.intBitsToFloat((int)0x00000000));
-		System.out.println(Float.intBitsToFloat((int)0xffffffff));
+		System.out.println("\tMIN VALUE BIT\t" + Float.intBitsToFloat((int)0x00000000));
+		System.out.println("\tMAX VALUE BIT\t" + Float.intBitsToFloat((int)0xffffffff));
 		System.out.println("\n\tSTOP FUNCTION\t\n");
 		System.out.println("\n\tEND\t\n");
 	}
