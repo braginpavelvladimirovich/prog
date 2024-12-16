cd4b33d6406ff88d6e38d1c2d2786033afe24cc0
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-12-16 16:46:11 +0700

    2024.12.16 arrays equals func

--- gm.java
+++ gm.java
@@ -13,7 +13,10 @@ class gm {
 		Integer [ ] n2 = { 1 , 2 , 3 , 4 } ;
 		Integer [ ] n3 = { 1 , 2 , 3 , 4 , 5 } ;
 		Integer [ ] n4 = { 1 , 3 , 2 , 4 , 5 } ;
-		if ( ae(n , n3))
+		String [ ] s = { " 0 1 2 3 " , " 0 1 2 3 " , " 0 1 2 3 4 " , " 0 1 3 2 4 " };
+		String [ ] s2 = { " 0 1 2 3 " , " 0 1 2 3 " , " 0 1 2 3 4 " , " 0 1 3 2 4 " };
+		String [ ] s3 = { " 0 1 2 3 " , " 0 1 2 3 " , " 0 1 2 3 4 " , " 0 2 2 2 4 " };
+		if ( ae(s3 , s2))
 			System.out.println("\n\tEQUAL\t\n");
 		else
 			System.out.println("\n\tNOT EQUAL\t\n");
