b2f769be2041d76710dd60b1c9456fba2e2a39f1
braginpavelvladimirovich <149386718+braginpavelvladimirovich@users.noreply.github.com>
2024-12-16 15:50:05 +0700

    2024.12.16 Gen done! numeric function universal object

--- gd.java
+++ gd.java
@@ -1,3 +1,58 @@
-class gen <A,B> {
+class Gen <T> {
+
+	T ob;
+
+
+	Gen(T o){
+		ob = o;
+	}
+
+	T getOb(){
+		return ob;
+	}
+
+	void showType(){
+		System.out.println("тип Т: " + ob.getClass().getName());
+	}
 }
-class gd
+class GenDemo{
+	public static void main(String[] args){
+
+		Gen <Integer> iOb;
+
+		iOb = new Gen <Integer> (100);
+
+
+		iOb.showType();
+		int v = iOb.getOb();
+		System.out.println("значение v: " + v);
+		System.out.println();
+
+
+		Gen <Double> dOb;
+
+		dOb = new Gen <Double> (100.1);
+
+
+		dOb.showType();
+		double x = dOb.getOb();
+		System.out.println("значение x: " + x);
+		System.out.println();
+
+
+
+		Gen <String> sOb;
+
+		sOb = new Gen <String> ("строка");
+
+		sOb.showType();
+		String s = sOb.getOb();
+		System.out.println("значение s: " + s);
+		System.out.println();
+
+
+
+
+	}
+
+}
--- gd2.java
+++ gd2.java
@@ -1,3 +1,55 @@
-class gen <A,B> {
+class TwoGen <T, V> {
+
+	T ob1;
+	V ob2;
+
+	TwoGen(T o1, V o2){
+		ob1 = o1;
+		ob2 = o2;
+
+	}
+	T getOb1(){
+		return ob1;
+	}
+
+	V getOb2(){
+		return ob2;
+	}
+
+	void showType(){
+		System.out.println("тип Т: " + ob1.getClass().getName());
+		System.out.println("тип V: " + ob2.getClass().getName());
+	}
 }
-class gd
+class TwoGenDemo{
+	public static void main(String[] args){
+		TwoGen <Integer, Double> tgOb;
+
+
+		tgOb = new TwoGen<Integer, Double> (100, 109.1);
+
+
+		tgOb.showType();
+		int v1 = tgOb.getOb1();
+		double v2 = tgOb.getOb2();
+		System.out.println("значение v1: " + v1);
+		System.out.println("значение v2: " + v2);
+		System.out.println();
+
+		TwoGen <Integer, String> tgOb2;
+
+		tgOb2 = new TwoGen<Integer, String> (100, "da");
+
+
+		tgOb2.showType();
+		int s1 = tgOb2.getOb1();
+		String s2 = tgOb2.getOb2();
+		System.out.println("значение s1: " + s1);
+		System.out.println("значение s2: " + s2);
+		System.out.println();
+
+
+
+	}
+
+}
